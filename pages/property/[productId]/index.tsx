6b2868e379f62808332ac04f640ee8a5e14363fe
MaltonLeo <your_github_khudaynazrovs@gmail.com>
2026-04-30 01:12:00 +0900

    feat: MUI customized integrations

--- pages/property/[productId]/index.tsx
+++ pages/property/[productId]/index.tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 const PropertyDetail = () => {
      const router = useRouter();
      const {productId} = router.query;
+       console.log("Property LIst COMPONENT- PAGES ROUTER")
     return <div>PROPERTY DETAIL {productId}</div>
 }
 
